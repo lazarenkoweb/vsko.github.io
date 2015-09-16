1792cee2916183a3bdde7d2d2b661d9766620922
vsko <info@zoom.net.ua>
2015-09-17 01:27:59 +0300

    added animation effect in project 2

--- markup/home/project2/js/common.js
+++ markup/home/project2/js/common.js
@@ -1,11 +1,11 @@
 jQuery(function($){
     $(document).on('click', '.portfolio .zoom', function(e) {
         e.preventDefault();
-        $('.overlay,.modal').show();
+        $('.overlay,.modal').animate({opacity: .9}, 300).show();
     });
 
     $(document).on('click', '.modal .close', function(e) {
         e.preventDefault();
-        $('.overlay,.modal').hide();
+        $('.overlay,.modal').animate({opacity: 0}, 100).hide();
     });
 }(jQuery));
