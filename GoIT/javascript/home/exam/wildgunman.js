cf2412c68dc6391eea379f1992170ed8f25365cf
Vitaliy Skorobagatko <info@zoom.net.ua>
2015-12-27 18:46:36 +0200

    update exam

--- GoIT/javascript/home/exam/wildgunman.js
+++ GoIT/javascript/home/exam/wildgunman.js
@@ -5,16 +5,19 @@
             'overlay': document.querySelector('.overlay'),
             'modal': document.querySelector('.modal'),
             'closeModal': document.getElementById('close-modal'),
-            'help': document.getElementById('help'),
             'play': document.querySelector('.button.play'),
+            'reset': document.getElementById('reset'),
+            'help': document.getElementById('help'),
             'screen': document.querySelector('.screen'),
             'aim': document.querySelector('.screen.aim'),
-            'message': document.querySelector('.message')
+            'message': document.querySelector('.message'),
         };
 
         this.person = 1;
         this.gunman = null;
         this.statusShot = false;
+        this.timerShot = null;
+        this.timerInterval = [];
 
         this.init();
     }
@@ -22,7 +25,7 @@
     WildGunman.prototype.init = function() {
         var __self = this;
 
-        //__self.Play();
+        //this.doPlay();
 
         this.dom.help.onclick = function(e) {
             __self.dom.modal.style.display = 'block';
@@ -34,94 +37,113 @@
         };
 
         this.dom.play.onclick = function(e) {
-            __self.Play();
+            __self.doPlay();
+        };
+
+        this.dom.reset.onclick = function(e) {
+            __self.doReset();
         };
 
         this.dom.aim.onclick = function(e) {
-            //console.log('x:' + e.layerX + ' y: ' + e.layerY);
-            //console.log();
-            //console.log(e);
-            //console.log(__self.statusShot);
-
-            // get element of shot
             var pointShot = e.toElement.className.split(' ')[0];
 
-            __self.sound('sfx/shot.m4a').play();
+            __self.sound('shot').play();
 
-            //switch(pointShot) {
-            //    case 'screen': __self.doGameOver(); break;
-            //    case 'gunman': __self.doShot(); break;
-            //}
+            if (__self.statusShot && pointShot === 'gunman') {
+                __self.doKilled();
+                __self.sound('win').play();
+                __self.doContinue();
+            } else {
+                __self.gameOver();
+            }
         };
     };
 
-    WildGunman.prototype.Play = function() {
+    WildGunman.prototype.doPlay = function() {
         var __self = this;
 
-        //this.sound('sfx/intro.m4a').play();
-
         this.dom.overlay.style.display = 'none';
+
+        //this.sound('intro').play();
+        __self.hideSay();
+
+        this.createGunman();
+
+        this.moveToDuel(function() {
+            __self.Duel();
+        });
+    };
+
+    WildGunman.prototype.doReset = function() {
+        this.dom.overlay.classList.remove('killed');
+        this.dom.overlay.classList.remove('continue');
+        this.dom.overlay.classList.add('played');
+
+        this.dom.play.classList.remove('replay');
+        this.dom.play.classList.remove('continue');
+        this.dom.play.classList.add('play');
+        this.dom.play.innerText = 'Play';
+
+        this.dom.overlay.style.display = 'block';
+
+        this.clearScreen();
+        this.clearAnimation();
+    };
+
+    WildGunman.prototype.gameOver = function() {
+        this.sound('death').play();
+
+        this.dom.overlay.classList.remove('played');
+        this.dom.overlay.classList.remove('continue');
+        this.dom.overlay.classList.add('killed');
+        this.dom.overlay.style.display = 'block';
+
         this.dom.play.classList.remove('play');
         this.dom.play.classList.add('replay');
         this.dom.play.innerText = 'Replay';
 
-        this.createGunman(this.dom.screen);
-
-        //__self.Killed();
-        //__self.Fire();
-
-        this.moveToDuel(function() {
-            __self.Duel();
-            //__self.Killed();
-        });
+        this.clearAnimation();
     };
 
-    WildGunman.prototype.Killed = function() {
-        var __self = this,
-            step = 10,
-            n = 0,
-            hat = this.gunman.cloneNode();
+    WildGunman.prototype.doContinue = function() {
+        this.dom.overlay.classList.remove('played');
+        this.dom.overlay.classList.add('continue');
+        this.dom.overlay.style.display = 'block';
 
-        var __timerId = setInterval(function() {
-            if (step === 12) {
-                clearInterval(__timerId);
-            }
-            __self.gunman.classList.add('step' + step);
-            if (step === 11) {
-                hat.classList.remove('step1');
-                hat.classList.add('step13');
-
-                __self.dom.screen.appendChild(hat);
-                var __timerId2 = setInterval(function() {
-                    if (n === 269) {
-                        clearInterval(__timerId2);
-                    }
-                    hat.style.top = -230+n + 'px';
-                    n++;
-                    //hat.style.right = parseInt(getComputedStyle(__self.gunman).right) + n/1.5 + 'px';
-                }, 5);
-            }
-            step++;
-
-        }, 200);
+        this.dom.play.classList.remove('play');
+        this.dom.play.classList.add('continue');
+        this.dom.play.innerText = 'Continue';
     };
 
-    WildGunman.prototype.createGunman = function(screen) {
+    WildGunman.prototype.createGunman = function() {
         var gunman = document.createElement('div');
         gunman.setAttribute('class', 'gunman gunman' + this.person + ' step1');
 
-        screen.innerHTML = '';
-        screen.appendChild(gunman);
-
+        this.dom.screen.innerHTML = '';
+        this.dom.screen.appendChild(gunman);
         this.gunman = gunman;
 
         return gunman;
     };
 
-    WildGunman.prototype.moveToDuel = function(action) {
+    WildGunman.prototype.removeGunman = function() {};
+
+    WildGunman.prototype.clearScreen = function() {
+        this.dom.screen.innerHTML = '';
+        clearTimeout(this.timerShot);
+    };
+
+    WildGunman.prototype.clearAnimation = function() {
+        for (var i=0; i<this.timerInterval.length; i++) {
+            clearInterval(this.timerInterval[i]);
+        }
+    };
+
+    WildGunman.prototype.moveToDuel = function(action, time) {
         var __self = this,
             n = 0,
-            step = 1;
+            step = 1,
+            time = time || 100;
 
         var __timerId = setInterval(function() {
             if (n === 33) {
@@ -141,36 +163,46 @@
                 __self.gunman.classList.add('step' + step);
                 __self.gunman.style.right = parseInt(getComputedStyle(__self.gunman).right) + 10 + 'px';
             }
-        }, 120);
+        }, time);
+
+        __self.timerInterval.push(__timerId);
     };
 
     WildGunman.prototype.Duel = function() {
         var __self = this;
 
-        this.sound('sfx/wait.m4a').play();
+        this.sound('wait').play();
 
-        setTimeout(this.Fire.bind(this), 3000);
+        this.timerShot = setTimeout(this.doFire.bind(this), 3000);
     };
 
-    WildGunman.prototype.Fire = function() {
+    WildGunman.prototype.doFire = function() {
+        var __self = this;
+
+        this.sound('fire').play();
+        this.say('Fire!!!', 570, 190);
+        this.statusShot = true;
+
+        this.timerShot = setTimeout(function() {
+            __self.doBang();
+            __self.gameOver();
+        }, 1000);
+    };
+
+    WildGunman.prototype.doBang = function() {
         var __self = this,
             n = 0,
             step = 6;
 
-        this.sound('sfx/fire.m4a').play();
-        this.message('Fire!!!', 570, 190);
-
-        this.statusShot = true;
+        this.statusShot = false;
 
         var __timerId = setInterval(function() {
             if (n === 4) {
                 clearInterval(__timerId);
-                __self.messageHide();
-                __self.GameOver();
+                __self.sound('shot-fall').play();
             } else {
                 n++;
                 step++;
-                console.log(step);
                 __self.gunman.classList.remove('step4');
                 __self.gunman.classList.remove('step7');
                 __self.gunman.classList.remove('step8');
@@ -178,37 +210,70 @@
                 __self.gunman.classList.add('step' + step);
             }
         }, 120);
+
+        __self.timerInterval.push(__timerId);
     };
 
-    WildGunman.prototype.GameOver = function() {
-        this.sound('sfx/death.m4a').play();
+    WildGunman.prototype.doKilled = function() {
+        var __self = this,
+            step = 10,
+            n = 0,
+            hat = this.gunman.cloneNode();
 
-        this.dom.overlay.classList.remove('nogame');
-        this.dom.overlay.classList.add('killed');
-        this.dom.overlay.style.display = 'block';
+        clearTimeout(this.timerShot);
 
-        this.statusShot = false;
+        var __timerId = setInterval(function() {
+            if (step === 12) {
+                clearInterval(__timerId);
+            }
+            __self.gunman.classList.add('step' + step);
+            if (step === 11) {
+                hat.classList.remove('step1');
+                hat.classList.add('step13');
+
+                __self.dom.screen.appendChild(hat);
+
+                var __timerId2 = setInterval(function() {
+
+                    if (n === 269) {
+                        clearInterval(__timerId2);
+                    }
+
+                    hat.style.top = -230+n + 'px';
+                    n++;
+                    //hat.style.right = parseInt(getComputedStyle(__self.gunman).right) + n/1.5 + 'px';
+                }, 5);
+
+                __self.timerInterval.push(__timerId2);
+            }
+            step++;
+        }, 200);
+
+        __self.timerInterval.push(__timerId);
+
+        this.hideSay();
     };
 
-    WildGunman.prototype.message = function(text, x, y) {
+    WildGunman.prototype.say = function(text, x, y) {
         var xPx = (x || 8) + 'px',
             yPx = (y || 120) + 'px';
 
+        this.dom.message.classList.add('arrow-left');
         this.dom.message.style.display = 'block';
         this.dom.message.style.top = yPx;
         this.dom.message.style.left = xPx;
         this.dom.message.innerText = text;
     };
 
-    WildGunman.prototype.messageHide = function() {
+    WildGunman.prototype.hideSay = function() {
         if (this.dom.message.style.display == 'block') {
-
             this.dom.message.style.display = 'none';
             this.dom.message.removeAttribute('style');
         }
     };
 
-    WildGunman.prototype.sound = function(sfx) {
+    WildGunman.prototype.sound = function(name) {
+        var sfx = 'sfx/' + name + '.m4a';
         return new Audio(sfx);
     };
 
@@ -228,95 +293,12 @@
     window.wildGunman = new WildGunman();
 })();
 
-
-
-
-
-
-
-
-
-
-
-
-
 /*
- WildGunman.prototype.duel = function() {
- var __self = this;
-
- this.countStep = 7;
-
- var __timerId = setTimeout(function() {
- __self.countNum--;
-
- //console.log('shot');
- //character.classList.add('step7');
- //character.classList.add('step8');
-
- if (__self.countNum === 0) {
- clearTimeout(__timerId2);
- }
-
- //__self.duel2();
-
- }, 10);
- };
+setTimeout
+    Duel        3000
+    doFire      1000
+setInterval
+    moveToDuel  100
+    doBang      120
+    doKilled    200 5
  */
-
-//WildGunman.prototype.moveToDuelOld = function(action) {
-//    var __self = this,
-//        stateRight = parseInt(getComputedStyle(this.gunman).right);
-//
-//    this.gunman.classList.remove('step1');
-//    this.gunman.classList.remove('step2');
-//    this.gunman.classList.remove('step3');
-//    this.gunman.style.right = stateRight + 10 + 'px';
-//
-//    var __timerId = setTimeout(function() {
-//        if (__self.step >= 3) {
-//            __self.step = 0;
-//        }
-//        __self.step++;
-//
-//        __self.moveToDuel(action);
-//
-//        __self.gunman.classList.add('step' + __self.step);
-//    }, 100);
-//
-//    if (stateRight >= 330) {
-//        this.gunman.classList.add('step4');
-//        clearTimeout(__timerId);
-//
-//        action();
-//    }
-//};
-//Function.prototype.repeat = function(num) {
-//    var n = 1,
-//        num = num || 1,
-//        __call = this;
-//
-//    var timerId = setInterval(function() {
-//        if (n <= num) {
-//            n++;
-//            __call();
-//        }
-//        else {
-//            clearInterval(timerId);
-//        }
-//    }, 100);
-//};
-//
-//WildGunman.prototype.repeatTo = function(num) {
-//    var n = 0,
-//        num = num || 1;
-//
-//    var timerId = setInterval(function() {
-//        if (n < num) {
-//            n++;
-//            console.log(n);
-//        }
-//        else {
-//            clearInterval(timerId);
-//        }
-//    }, 100);
-//};
